cd0b6a21292668e03d6857691e82af943e6b33eb
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-05-25 17:56:01 -0300

    Dev: GameScreen page

---
+++ src/pages/Game.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import propTypes from 'prop-types';
+
+class Game extends Component {
+  render() {
+    return (
+      <div>
+        <h1>GameScreen</h1>
+      </div>
+    );
+  }
+}
+
+export default Game;
