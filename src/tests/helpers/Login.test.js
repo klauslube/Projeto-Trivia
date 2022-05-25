b58f7d1b39c93e81e15b4cbddf44a2a443cc5165
klauslube <klauslube93@gmail.com>
2022-05-24 21:52:39 -0300

    Adicionado teste de redirecionamento (incompleto)

--- src/tests/helpers/Login.test.js
+++ src/tests/helpers/Login.test.js
@@ -27,59 +27,77 @@ describe('Testa a page Login', () => {
     const Input =  screen.getAllByRole('textbox');
     expect(Input[0] && Input[1]).toBeInTheDocument();
   })
-  test('testa se existe um elemento button na página',() => {
+  test('testa se existe um elemento button play e settings na página',() => {
     renderWithRouterAndRedux(<App />, initialState);
 
-    const button = screen.getByRole('button');
-    expect(button).toBeInTheDocument();
+    const button = screen.getAllByRole('button');
+    expect(button[0] && button[1]).toBeInTheDocument();
   })
-  test('testa se o elemento button esta desabilitado caso os inputs não estejam preenchidos',() => {
+  test('testa se o elemento button play esta desabilitado caso os inputs não estejam preenchidos',() => {
     renderWithRouterAndRedux(<App />, initialState);
 
     const Input = screen.getAllByRole('textbox');
     expect(Input[0] && Input[1]).toHaveValue('');
-    const button = screen.getByRole('button');
-    expect(button).toBeDisabled();
+    const button = screen.getAllByRole('button');
+    expect(button[0]).toBeDisabled();
   })
-  test('testa se o elemento button esta desabilitado caso o user não está preenchido',() => {
+  test('testa se o elemento button play esta desabilitado caso o user não está preenchido',() => {
     renderWithRouterAndRedux(<App />, initialState);
 
     const Input = screen.getAllByRole('textbox');
-    const button = screen.getByRole('button');
+    const button = screen.getAllByRole('button');
 
     userEvent.type(Input[0], INVALID_USER);
-    expect(button).toBeDisabled();
+    expect(button[0]).toBeDisabled();
   })
-  test('testa se o elemento button esta desabilitado caso o email não está preenchido de forma correta',() => {
+  test('testa se o elemento button play esta desabilitado caso o email não está preenchido de forma correta',() => {
     renderWithRouterAndRedux(<App />, initialState);
 
     const Input = screen.getAllByRole('textbox');
-    const button = screen.getByRole('button');
+    const button = screen.getAllByRole('button');
 
     userEvent.type(Input[0], VALID_USER);
     userEvent.type(Input[1], INVALID_EMAIL_0);
-    expect(button).toBeDisabled();
+    expect(button[0]).toBeDisabled();
 
     userEvent.type(Input[0], VALID_USER);
     userEvent.type(Input[1], INVALID_EMAIL_1);
-    expect(button).toBeDisabled();
+    expect(button[0]).toBeDisabled();
 
     userEvent.type(Input[0], VALID_USER);
     userEvent.type(Input[1], INVALID_EMAIL_2);
-    expect(button).toBeDisabled();
+    expect(button[0]).toBeDisabled();
 
     userEvent.type(Input[0], VALID_USER);
     userEvent.type(Input[1], INVALID_EMAIL_3);
-    expect(button).toBeDisabled();
+    expect(button[0]).toBeDisabled();
   })
-  test('testa se o elemento button é habilitado após os inputs user e email serem preenchidos ',() => {
+  test('testa se o elemento button play é habilitado após os inputs user e email serem preenchidos ',() => {
     renderWithRouterAndRedux(<App />, initialState);
 
     const Input = screen.getAllByRole('textbox');
-    const button = screen.getByRole('button');
+    const button = screen.getAllByRole('button');
     userEvent.type(Input[0], VALID_USER);
     userEvent.type(Input[1], VALID_EMAIL);
-    expect(button).toBeEnabled();
+    expect(button[0]).toBeEnabled();
+  })
+  test('testa se ao clicar no elemento button play o usuario é redirecionado para a página do jogo', () => {
+    const { history } = renderWithRouterAndRedux(<App />, initialState);
+
+    const button = screen.getAllByRole('button');
+    userEvent.type(Input[0], VALID_USER);
+    userEvent.type(Input[1], VALID_EMAIL);
+    userEvent.click(button[0]);
+    expect(history.location.pathname).toBe('/jogo')
+     
+   })
+  test('testa se ao clicar no elemento button settings o usuario é redirecionado para a página de configurações', () => {
+   const { history } = renderWithRouterAndRedux(<App />, initialState);
+
+    const button = screen.getAllByRole('button');
+    userEvent.click(button[1]);
+    expect(history.location.pathname).toBe('/configurações')
+
   })
   
 })
