c1fb9ed939e2b8a71882bd0582fb6cc7aa929822
klauslube <klauslube93@gmail.com>
2022-05-26 10:51:29 -0300

    commit inicial

---
+++ src/tests/helpers/Game.test.js
@@ -0,0 +1,5 @@
+import React from "react";
+import renderWithRouterAndRedux from "./renderWithRouterAndRedux";
+import { screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from '../../App'
