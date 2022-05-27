ba8c9165f99c7da56895342517fad12c2a599b1d
Anthony Cruz <tony_esqueloto@hotmail.com>
2022-05-27 20:35:56 -0300

    adiciona requisitos simples:15, 16 e 18 completos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Settings from './pages/Settings';
 import Game from './pages/Game';
 import Feedback from './pages/Feedback';
+import Ranking from './pages/Ranking';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
         <Route exact path="/settings" component={ Settings } />
         <Route exact path="/game" component={ Game } />
         <Route path="/feedback" component={ Feedback } />
+        <Route path="/ranking" component={ Ranking } />
       </Switch>
 
     </div>
